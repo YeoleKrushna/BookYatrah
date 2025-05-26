411aea104100e8374be789d4e4a389842c722952
YeoleKrushna <yeolekrushna00@gmail.com>
2025-05-26 15:09:41 +0530

    Fixed Errors

--- BookYatrah/settings.py
+++ BookYatrah/settings.py
@@ -156,9 +156,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = 'infymediaaa@gmail.com'           # Your Gmail address
-EMAIL_HOST_PASSWORD = 'nacc fosh lglm blwo'         # Your Gmail App Password
+from decouple import config
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')         # Your Gmail App Password
 
 
 # Optional security improvements for production
